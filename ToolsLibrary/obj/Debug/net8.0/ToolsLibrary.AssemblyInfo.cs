2c6ae4bfb75febd2b0512068e0a834db41e4bdf7
Juan P. Torres Torres <Juan.TorresT@hacienda.pr.gov>
2024-05-09 12:18:32 -0400

    Improment.

--- ToolsLibrary/obj/Debug/net8.0/ToolsLibrary.AssemblyInfo.cs
+++ ToolsLibrary/obj/Debug/net8.0/ToolsLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolsLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0a8b6a1dc42bf1c711c42f1db4902d38dd0540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0021026f9140de01fb64562da401ad1bcbf51430")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolsLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolsLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
